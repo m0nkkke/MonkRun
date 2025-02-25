5658ddc51dbf7b05fcdc45c05b2c0ec6a2e9562a
Alangmat <alangmator@gmail.com>
2025-02-25 22:55:01 +0700

    Monk colliders were reworked (Created head and back)

--- Assets/Scripts/Monk/CollisionScript.cs
+++ Assets/Scripts/Monk/CollisionScript.cs
@@ -6,36 +6,26 @@ public class CollisionScript : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        //print("бац");
         if (collision.gameObject.tag == "RockCollider")
         {
             print("Rock");
             GameManager.Instance.Score = 0;
         }
-        else if (collision.gameObject.tag == "Spider")
-        {
-            print("Spider");
-            GameManager.Instance.Score = 0;
-        }
-        else if (collision.gameObject.tag == "Fly")
-        {
-            print("Fly");
-            GameManager.Instance.Score = 0;
-        }
+
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.name == "PitTrigger")
-        {
-            print("Pit");
-            GameManager.Instance.Score = 0;
-        }
-        else if (other.gameObject.name == "RiverTrigger")
-        {
-            print("River");
-            GameManager.Instance.Score = 0;
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.name == "PitTrigger")
+    //    {
+    //        print("Pit");
+    //        GameManager.Instance.Score = 0;
+    //    }
+    //    else if (other.gameObject.name == "RiverTrigger")
+    //    {
+    //        print("River");
+    //        GameManager.Instance.Score = 0;
+    //    }
+    //}
     private void Update()
     {
         if (GameManager.Instance.Score > 10)
