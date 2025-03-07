38411e0f8351c5505f3b498aabf13786b8272f88
m0nkkke <embr4ce02@gmail.com>
2025-03-07 14:40:42 +0700

    Pre-final day/night cycle system

--- Assets/DayNightCycle/DayCycleManager.cs
+++ Assets/DayNightCycle/DayCycleManager.cs
@@ -1,10 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DayCycleManager : MonoBehaviour
 {
-    [Range (0, 1)]
+    [Range(0, 1)]
     public float TimeOfDay;
     public float DayDuration = 30f;
 
@@ -21,19 +20,66 @@ public class DayCycleManager : MonoBehaviour
     private float sunIntensity;
     private float moonIntensity;
 
-    // Start is called before the first frame update
+    private bool isTransitioning = false; // Флаг перехода
+    private float targetTimeOfDay; // Целевое время суток
+    private int nextScoreThreshold = 10; // Следующий порог очков
+
     void Start()
     {
         sunIntensity = Sun.intensity;
         moonIntensity = Moon.intensity;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        TimeOfDay += Time.deltaTime / DayDuration;
-        if (TimeOfDay > 1) TimeOfDay -= 1;
+        if (!isTransitioning && GameManager.Instance.Score >= nextScoreThreshold)
+        {
+            nextScoreThreshold += 10; // порог смены
+            ChangeDayNight();
+        }
 
+        if (isTransitioning)
+        {
+            UpdateLighting();
+        }
+    }
+
+    private void ChangeDayNight()
+    {
+        float newTime = (TimeOfDay < 0.5f) ? 0.75f : 0.25f; // день / ночь
+        TriggerTimeChange(newTime);
+    }
+
+    public void TriggerTimeChange(float targetTime)
+    {
+        if (isTransitioning) return;
+
+        targetTimeOfDay = targetTime;
+        StartCoroutine(FastTransition(targetTimeOfDay));
+    }
+
+    private IEnumerator FastTransition(float targetTime)
+    {
+        isTransitioning = true;
+        float startTime = TimeOfDay;
+        float elapsedTime = 0f;
+        float duration = 2f; // длительность перехода
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            TimeOfDay = Mathf.Lerp(startTime, targetTime, elapsedTime / duration);
+            UpdateLighting();
+            yield return null;
+        }
+
+        TimeOfDay = targetTime;
+        UpdateLighting();
+        isTransitioning = false;
+    }
+
+    private void UpdateLighting()
+    {
         RenderSettings.skybox.Lerp(NightSkybox, DaySkybox, SkyCurve.Evaluate(TimeOfDay));
         DynamicGI.UpdateEnvironment();
 
@@ -41,6 +87,6 @@ public class DayCycleManager : MonoBehaviour
         Sun.intensity = sunIntensity * SunCurve.Evaluate(TimeOfDay);
 
         Moon.transform.localRotation = Quaternion.Euler(TimeOfDay * 360f + 180, 0, 180);
-        Moon.intensity = moonIntensity * MoonCurve.Evaluate(TimeOfDay);
+        Moon.intensity = 0.7f * MoonCurve.Evaluate(TimeOfDay);
     }
 }
