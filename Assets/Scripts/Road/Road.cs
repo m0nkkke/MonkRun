48c3b59a7ca6658fedcfafed889dfae8d3f539f0
Alangmat <alangmator@gmail.com>
2025-03-25 21:27:00 +0700

    Created segments and Script of Road

---
+++ Assets/Scripts/Road/Road.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    //private void Update()
+    //{
+    //    DestroyRoad();
+    //}
+
+    //// Update is called once per frame
+    //void FixedUpdate()
+    //{
+    //    Move();
+    //}
+
+    //private void Move()
+    //{
+    //    transform.Translate(-transform.right * GameManager.Instance.roadSpeed * Time.fixedDeltaTime);
+
+    //}
+    //private void DestroyRoad()
+    //{
+    //    if (transform.position.x < -15f)
+    //    {
+    //        GameObject spawnerObject = GameObject.Find("Spawner");
+    //        RoadSpawner roadSpawner = spawnerObject.GetComponent<RoadSpawner>();
+    //        roadSpawner.AllRoads.RemoveAt(0);
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
